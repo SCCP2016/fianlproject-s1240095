5ec3eeee7627ef75c0ab1503cafb508043c1e7e1
s1240095 <s1240095@u-aizu.ac.jp>
2016-07-21 20:43:28 +0900

    I don't know what is changed.

--- src/CommandParser.rb
+++ src/CommandParser.rb
@@ -6,7 +6,13 @@ attr_reader :argv
   end
 
   def parse
-    if @argv[0] == "generate" then
+    if (@argv[0] == "g")||(@argv[0] == "generate") then
+
+    elsif (@argv[0] == "s")||(@argv[0] == "submit") then
+
+    elsif (@argv[0] == "l")||(@argv[0] == "list") then
+
+    else
 
     end
   end

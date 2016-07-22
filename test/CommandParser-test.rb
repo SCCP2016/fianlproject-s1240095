43038adddacb8f9abedfdbab88f57e0e917283df
s1240095 <s1240095@u-aizu.ac.jp>
2016-07-22 14:29:01 +0900

    Change "generate" parser test

--- test/CommandParser-test.rb
+++ test/CommandParser-test.rb
@@ -6,5 +6,10 @@ class CommandParserTest < Test::Unit::TestCase
     argv=["generate","Prog0","-h","Ex","-format","1","-m","13"]
     cmdpr = CommandParser.new(argv)
     assert_equal 8, cmdpr.argv.count
+    assert_equal "generate", cmdpr.parse.type
+    assert_equal "Prog0", cmdpr.parse.dir_name
+    assert_equal "Ex", cmdpr.parse.header
+    assert_equal 1, cmdpr.parse.format
+    assert_equal 13, cmdpr.parse.max
   end
 end

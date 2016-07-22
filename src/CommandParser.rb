9405f207d459fdefcd9707120a2be82a55d28df7
s1240095 <s1240095@u-aizu.ac.jp>
2016-07-22 14:02:17 +0900

    Add "generate" command Perser

--- src/CommandParser.rb
+++ src/CommandParser.rb
@@ -7,7 +7,29 @@ attr_reader :argv
 
   def parse
     if (@argv[0] == "g")||(@argv[0] == "generate") then
-
+exdir = @argv[1]
+i=2
+dir_h = "Ex"
+fmt = 1
+max = 13
+while i < @argv.count do
+if @argv[i][0] == "-" then
+if (@argv.count > i) && (@argv[i+1] != "-") then
+case @argv[i][1]
+when "h" then
+dir_h = @argv[i+1]
+when "f" then
+fmt = @argv[i+1]
+when "m" then
+max = @argv[i+1]
+end
+i += 2
+end
+else
+raise "CommandPerseエラー"
+end
+end
+end
     elsif (@argv[0] == "s")||(@argv[0] == "submit") then
 
     elsif (@argv[0] == "l")||(@argv[0] == "list") then
